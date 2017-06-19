f7fc0f332e8f96f45fc8d02769153fe552de7736
Sebastian Wolfertz <sebastian@wolfertz.net>
2017-06-19 10:50:40 +0200

    Pixel Based responsive Picture

--- Classes/ViewHelpers/ResponsivePictureViewhelper.php
+++ Classes/ViewHelpers/ResponsivePictureViewhelper.php
@@ -10,72 +10,37 @@ use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 class ResponsivePictureViewHelper extends ImageViewHelper
 {
 	use CompileWithRenderStatic;
-
 	/**
 	 * Resizes the image (if required) and returns its path. If the image was not resized, the path will be equal to $src
 	 *
-	 * @param array                     $arguments
-	 * @param \Closure                  $renderChildrenClosure
+	 * @param array $arguments
+	 * @param \Closure $renderChildrenClosure
 	 * @param RenderingContextInterface $renderingContext
-	 *
 	 * @return string
 	 * @throws Exception
 	 */
-	public static function renderStatic(
-		array $arguments,
-		\Closure $renderChildrenClosure,
-		RenderingContextInterface $renderingContext
-	) {
-		$sources = $arguments['sources'];
-		$src     = "";
-		foreach ($sources as $sourcekey => $source)
+	public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+	{
+		$output = "";
+
+		foreach ($arguments['sources'] as $source)
 		{
 			$arguments['cropVariant'] = $source['variant'];
-			if (!empty($source['query']))
+			foreach ($source["widths"] as $width)
 			{
-				$src .= "media=\"" . $source['query'] . "\" ";
-			}
-			$src .= "<source ";
-			$src .= "sizes=\"" . $source['sizes'] . "\" srcset=\"";
-			$src .= static::getSrcSet($source, false, $arguments, $renderChildrenClosure, $renderingContext);
-			$src .= "\" />";
-			if ($source === end($sources))
-			{
-				$src .= "<img alt=\"" . $arguments['alt'] . "\" src = \"";
-				$src .= static::getSrcSet($source, true, $arguments, $renderChildrenClosure, $renderingContext);
-				$src .= "\" />";
-			}
-		}
+				$output .= "<source";
+				$output .= " media=\"(max-width: ".$width."px)\"";
 
-		return $src;
-	}
-
-	protected static function getSrcset($source, $single, $arguments, $renderChildrenClosure, $renderingContext)
-	{
-		$divider = "";
-		$src     = "";
-		foreach ($source['width'] as $width)
-		{
-			if ($width === end($source['width']) && $single)
-			{
+				$output .= " srcset=\"";
 				$arguments['maxWidth'] = $width;
-				$src .= parent::renderStatic($arguments, $renderChildrenClosure, $renderingContext);
-			}
-			elseif (!$single)
-			{
-				$arguments['maxWidth'] = $width;
-				$src .= $divider . parent::renderStatic($arguments, $renderChildrenClosure,
-						$renderingContext) . " " . $width . "w";
-				$divider               = ", ";
+				$output .= parent::renderStatic($arguments, $renderChildrenClosure, $renderingContext) . " 1x";
 				$arguments['maxWidth'] = $width * 2;
-				$src .= $divider . parent::renderStatic($arguments, $renderChildrenClosure,
-						$renderingContext) . " " . ($width * 2) . "w";
+				$output .= ", " . parent::renderStatic($arguments, $renderChildrenClosure, $renderingContext) . " 2x";
+				$output .= "\">";
 			}
 		}
-
-		return $src;
+		return $output;
 	}
-
 
 	/**
 	 * Initialize arguments.
